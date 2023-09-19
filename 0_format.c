8940e912f7779d63c63dd02d593eeb4d4fc51600
Bukky Pase <olubukunolapase@yahoo.com>
2023-09-19 12:35:40 +0100

    Task 0

--- 0_format.c
+++ 0_format.c
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
     {
         if (*format != '%') /* check if the character is not ampersand */
         {
-            putchar(*format);
+            _putchar(*format);
             output++; /* print the character in increments*/
         }
         else
@@ -32,7 +32,7 @@ int _printf(const char *format, ...)
             if (*format == 'c') /* if it is a single character i.e. 'c' print to stdout and increment count */
             {
                 char c = va_arg(args, int);
-                putchar(c);
+                _putchar(c);
                 output++;
             }
             else if (*format == 's') /* if it is a string i.e. 's', calculate the string length and print the string to stdout via looping and increment count */
@@ -41,25 +41,25 @@ int _printf(const char *format, ...)
                 length = strlen(string);
                 for (i = 0; i < length; i++)
                 {
-                    putchar(string[i]);
+                    _putchar(string[i]);
                     output++;
                 }
             }
             else if (*format == '%') /* if it is ampersand i.e. '%' print a literal ampersand to stdout and increment count */
             {
-                putchar('%');
+                _putchar('%');
                 output++;
             }
             else /* if the specifier is unknown print ampersand and the unknown specifier */
             {
-                putchar('%');
+                _putchar('%');
                 output++;
-                putchar(*format);
+                _putchar(*format);
                 output++;
             }
         }
         format++; 
     }
     va_end(args);
-    return output; /* return the total number 
+    return output; /* return the total number */ 
 }
